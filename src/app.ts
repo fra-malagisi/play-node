5e999e9bf1bae7aa420c269c874fbbc0a818b105
francesco.malagisi <francesco.malagisi@accenture.com>
2022-03-25 14:27:26 +0100

    fix: add airbnb-config

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { corsUrl } from './config';
+import { NotFoundError } from './core/ApiError';
+
 /**
  *  TODO:
- *  - add logger
+ *  - Add Logger
  */
 
 const app = express();
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }));
 app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
+
+/**
+ *  TODO:
+ *  - Add Routes
+ */
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => next(new NotFoundError()));
 
 app.get('/', (req, res) => {
   res.send('Well done!');
