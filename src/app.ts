4d317155f0db1930288b1162fc8714040bac3d05
francesco.malagisi <francesco.malagisi@accenture.com>
2022-03-28 11:42:09 +0200

    feat: middleware error handling

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { corsUrl } from './config';
-import { NotFoundError } from './core/ApiError';
+import { corsUrl, environment } from './config';
+import { ApiError, InternalError, NotFoundError } from './core/ApiError';
 
 /**
  *  TODO:
@@ -22,8 +22,17 @@ app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(new NotFoundError()));
 
-app.get('/', (req, res) => {
-  res.send('Well done!');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ApiError) {
+    ApiError.handle(err, res);
+  } else {
+    if (environment === 'development') {
+      // Logger.error(err);
+      return res.status(500).send(err.message);
+    }
+    ApiError.handle(new InternalError(), res);
+  }
 });
 
 app.listen(3000, () => {
